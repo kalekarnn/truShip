df47571716ea0537f8cb0088ac82cec0d2f9671e
NA014KA <narendra.kalekar@pb.com>
2019-09-28 10:11:28 +0530

    first commit

---
+++ src/main/java/com/pb/shipping/hackathon/truship/TrushipApplication.java
@@ -0,0 +1,13 @@
+package com.pb.shipping.hackathon.truship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrushipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrushipApplication.class, args);
+	}
+
+}
