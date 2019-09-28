92c1e6d503ef671a11a04fa2af34df757eeb9eb1
NA014KA <narendra.kalekar@pb.com>
2019-09-28 14:05:34 +0530

    get vehicle

--- src/main/java/com/pb/shipping/hackathon/truship/rest/VehicleController.java
+++ src/main/java/com/pb/shipping/hackathon/truship/rest/VehicleController.java
@@ -2,11 +2,11 @@ package com.pb.shipping.hackathon.truship.rest;
 
 import com.pb.shipping.hackathon.truship.model.Vehicle;
 import com.pb.shipping.hackathon.truship.service.VehicleService;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/vehicle")
@@ -18,5 +18,15 @@ public class VehicleController {
     @PostMapping
     public Vehicle createVehicle(@RequestBody Vehicle vehicle) {
         return vehicleService.creteVehicle(vehicle);
+    }
+
+    @GetMapping
+    public List<Vehicle> getAll() {
+        return vehicleService.getAll();
+    }
+
+    @GetMapping("/{regNo.}")
+    public Vehicle getVehicle(@PathVariable("regNo") long regNo) throws NotFound {
+        return vehicleService.getVehicleByRegNo(regNo);
     }
 }
--- src/main/java/com/pb/shipping/hackathon/truship/service/VehicleService.java
+++ src/main/java/com/pb/shipping/hackathon/truship/service/VehicleService.java
@@ -2,8 +2,12 @@ package com.pb.shipping.hackathon.truship.service;
 
 import com.pb.shipping.hackathon.truship.model.Vehicle;
 import com.pb.shipping.hackathon.truship.repo.VehicleRepository;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleService {
@@ -14,5 +18,13 @@ public class VehicleService {
     public Vehicle creteVehicle(Vehicle vehicle) {
 
         return vehicleRepository.save(vehicle);
+    }
+
+    public List<Vehicle> getAll(){
+        return  vehicleRepository.findAll();
+    }
+
+    public Vehicle getVehicleByRegNo(long regNo) throws NotFound {
+        return vehicleRepository.findById(regNo).orElseThrow(NotFound::new);
     }
 }
